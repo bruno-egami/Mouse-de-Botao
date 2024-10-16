2f942356e216daeb378fb668fe6741a7e00ec070
Bruno Kenji Nishitani Egami <132298821+bruno-egami@users.noreply.github.com>
2024-10-15 23:20:20 -0300

    Update mouse_botao.ino
    inclusão da função scroll

--- mouse_botao.ino
+++ mouse_botao.ino
@@ -1,58 +1,46 @@
 #include <Mouse.h>
-
+#include <Keyboard.h>
 
 /*
-  Mouse de botoes
+  Mouse de botoes com funcionalidade: movimento do cursor, clique, clique duplo, clique preso, clique direito, scroll bidirecional e ajuste de velocidade (cursor e scroll)
   Para placas de arduino micro, Leonardo, Pro Micro and Due boards only.
   O movimento do mouse é sempre relativo. 
   
-  Código de exemplo criado originalmente por Tom Igoe disponivel em:
-  http://www.arduino.cc/en/Tutorial/ButtonMouseControl
-
-  A implementação das demais funções e adequações foi realizada por bolsistas do projeto CRTA.
-  Mais informações estão disponíveis em: https://cta.ifrs.edu.br/
+  Código de exemplo criado originalmente por Tom Igoe e modificado por bolsistas do projeto CRTA.
+  
 */
 
+#define DEBOUNCING 50
+#define TEMPO 200
+#define MULTIPLI 0.08
+#define MULTIPLICA 1023
+#define SCROLL_SPEED_LOW 2
+#define SCROLL_SPEED_MEDIUM 5
+#define SCROLL_SPEED_HIGH 10
 
-#define DEBOUNCING 50     //tempo(millisegundos) para o DEBOUNCING
-#define TEMPO 200         //tempo para fazer a funcao de acelerar o mouse(valor em milissegundos)
-#define MULTIPLI 0.08     //Variar entre 0.001 e 0.08 para acelerar o avanco do mouse apos 800 milissegundos segurados
-#define MULTIPLICA 1023      //Velocidade maxima que o mouse tera no maximo do joystick(0-1023)
+// Pinos dos botões
+const int upButton = 2;
+const int downButton = 3;
+const int leftButton = 5;
+const int rightButton = 4;
+const int mouseButton = 6;
+const int rightmouseButton = 7;
+const int leftLong = 8;
+const int doubleleft = 9;
+const int ScrollButton = 10;
+const int velocidade = 11;
 
-//dizendo ao programa o numero dos pinos conectados aos botoes
-
-// Para utilizar os modulos analogicos, descomentar esta secao
-//const int analogico1 = A0; As portas analógicas somente serão utilizadas em conjunto com o módulo analógico
-//const int analogico2 = A1;
-
-// Botoes de acao
-const int upButton = 2; //movimento acima 
-const int downButton = 3; //movimento abaixo
-const int leftButton = 5; //movimento esquerda
-const int rightButton = 4; //movimento direito
-
-const int mouseButton = 6; //clique esquerdo
-const int rightmouseButton = 7; //clique direito
-const int leftLong = 8; //clique esquerdo preso
-const int doubleleft = 9; //clique esquer duplo
-const int StickButton = 10; //botao scrool
-const int velocidade = 11; //botao para alternar velocidade de movimento
-
-int range = 1;              // multiplicador do movimento do mouse em X e Y, quanto maior o numero mais rapido fica o mouse
-int responseDelay = 10;     // tempo de resposta do mouse(quanto maior o numero mais lento o mouse fica),numero em milisegundos
+int range = 1;
+int responseDelay = 10;
 boolean variavel = 0, variavel2 = 1, variavel3 = 1, variavel4 = 1, variavel5 = 1, variavel6 = 1;
 int d = 0;
 unsigned long espaco = 0, a = 0;
-float  t = 0.1;
+float t = 0.1;
+boolean scrollMode = false;
+int scrollSpeed = SCROLL_SPEED_MEDIUM;
+int scrollSpeedLevel = 1; // 0 = baixo, 1 = médio, 2 = alto
 
 void setup() {
-  // inicializa as variaveis como entradas
-
-  // Para utilizar os modulos analogicos, descomentar esta secao
-  //pinMode(analogico1, INPUT);
-  //pinMode(analogico2, INPUT);
-
-  
   pinMode(upButton, INPUT_PULLUP);
   pinMode(downButton, INPUT_PULLUP);
   pinMode(leftButton, INPUT_PULLUP);
@@ -61,47 +49,100 @@ void setup() {
   pinMode(leftLong, INPUT_PULLUP);
   pinMode(doubleleft, INPUT_PULLUP);
   pinMode(rightmouseButton, INPUT_PULLUP);
-  pinMode(StickButton, INPUT_PULLUP);
+  pinMode(ScrollButton, INPUT_PULLUP);
   pinMode(velocidade, INPUT_PULLUP);
   
-  // inicializa a biblioteca do mouse.h
   Mouse.begin();
+  Keyboard.begin();
   Serial.begin(9600);
 }
 
 void loop() {
-  // Este loop ? infinito,e em cada vez que o loop reinicia ? feito a leitura dos bot?es
-  int  xDistance = 0;
-  int  yDistance = 0;
+  int xDistance = 0;
+  int yDistance = 0;
 
-  boolean upState = !(digitalRead(upButton));
-  boolean downState = !(digitalRead(downButton));
-  boolean rightState = !(digitalRead(rightButton));
-  boolean leftState = !(digitalRead(leftButton));
+  boolean upState = !digitalRead(upButton);
+  boolean downState = !digitalRead(downButton);
+  boolean rightState = !digitalRead(rightButton);
+  boolean leftState = !digitalRead(leftButton);
 
-  if (((upState | downState | rightState | leftState) == 1) && d == 0) {
-    a = millis();
-    d = 1;
-  }
-
-  if (((upState | downState | rightState | leftState) == 0) && d == 1) {
-    a = 0;
-    d = 0;
-  }
-
-  if ((a + TEMPO) < (millis()) && d == 1) {
-    t = t + MULTIPLI;
-    xDistance = (rightState - leftState) * t;
-    yDistance = (downState - upState) * t;
+  // Controle do modo de scroll
+  if (digitalRead(ScrollButton) == LOW) {
+    if (variavel6 == 1) {
+      delay(DEBOUNCING);
+      if (digitalRead(ScrollButton) == LOW) {
+        scrollMode = !scrollMode; // Alterna entre modo normal e modo de scroll
+        variavel6 = 0;
+      }
+    }
   } else {
-    xDistance = (rightState - leftState) * range;
-    yDistance = (downState - upState) * range;
-    t = range;
+    variavel6 = 1;
   }
-  if ((xDistance != 0) || (yDistance != 0)) {
-    Mouse.move(xDistance, yDistance, 0);
+
+  // Controle da velocidade de scroll e movimento
+  if ((digitalRead(velocidade) == LOW) && (espaco + 1000) < millis()) {
+    espaco = millis();
+    scrollSpeedLevel = (scrollSpeedLevel + 1) % 3;
+    switch (scrollSpeedLevel) {
+      case 0:
+        scrollSpeed = SCROLL_SPEED_LOW;
+        range = 2;
+        break;
+      case 1:
+        scrollSpeed = SCROLL_SPEED_MEDIUM;
+        range = 4;
+        break;
+      case 2:
+        scrollSpeed = SCROLL_SPEED_HIGH;
+        range = 8;
+        break;
+    }
+    Serial.print("Scroll/Move Speed: ");
+    Serial.println(scrollSpeed);
   }
- //se o botao esquerdo ? apertado
+
+  if (scrollMode) {
+    // Modo de scroll bidirecional
+    int scrollY = (downState - upState) * scrollSpeed;
+    int scrollX = (rightState - leftState) * scrollSpeed;
+
+    if (scrollY != 0) {
+      Mouse.move(0, 0, scrollY);
+    }
+
+    if (scrollX != 0) {
+      // Simula o scroll horizontal
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Mouse.move(0, 0, scrollX);
+      Keyboard.release(KEY_LEFT_SHIFT);
+    }
+  } else {
+    // Modo de movimento normal do mouse
+    if (((upState | downState | rightState | leftState) == 1) && d == 0) {
+      a = millis();
+      d = 1;
+    }
+
+    if (((upState | downState | rightState | leftState) == 0) && d == 1) {
+      a = 0;
+      d = 0;
+    }
+
+    if ((a + TEMPO) < (millis()) && d == 1) {
+      t = t + MULTIPLI;
+      xDistance = (rightState - leftState) * t;
+      yDistance = (downState - upState) * t;
+    } else {
+      xDistance = (rightState - leftState) * range;
+      yDistance = (downState - upState) * range;
+      t = range;
+    }
+    if ((xDistance != 0) || (yDistance != 0)) {
+      Mouse.move(xDistance, yDistance, 0);
+    }
+  }
+
+  // Controle do botão esquerdo do mouse
   if (digitalRead(mouseButton) == LOW) {
     if (variavel2 == 1) {
       delay(DEBOUNCING);
@@ -111,21 +152,13 @@ void loop() {
       }
     }
   } else {
-    variavel2 = 1;
-  }
-  if (digitalRead(StickButton) == LOW) {
-    if (variavel6 == 1) {
-      delay(DEBOUNCING);
-      if (digitalRead(StickButton) == LOW) {
-        Mouse.press(MOUSE_LEFT);
-        variavel6 = 0;
-      }
+    if (variavel2 == 0) {
+      Mouse.release(MOUSE_LEFT);
+      variavel2 = 1;
     }
-  } else {
-    variavel6 = 1;
   }
 
-  // se o botao direito ? apertado
+  // Controle do botão direito do mouse
   if (digitalRead(rightmouseButton) == LOW) {
     if (variavel3 == 1) {
       delay(DEBOUNCING);
@@ -135,55 +168,43 @@ void loop() {
       }
     }
   } else {
-    Mouse.release(MOUSE_RIGHT);
-    variavel3 = 1;
+    if (variavel3 == 0) {
+      Mouse.release(MOUSE_RIGHT);
+      variavel3 = 1;
+    }
   }
 
-  //se o botao de clique duplo ? apertado
+  // Controle de duplo clique
   if (digitalRead(doubleleft) == LOW) {
     if (variavel4 == 1) {
       delay(DEBOUNCING);
       if (digitalRead(doubleleft) == LOW) {
-        // muda o estado l?gico do bot?o
-        if (!Mouse.isPressed(MOUSE_LEFT)) {
-          Mouse.click(MOUSE_LEFT);
-          delay(200);
-          Mouse.click(MOUSE_LEFT);
-          delay(500);
-          variavel4 = 0;
-        }
+        Mouse.click(MOUSE_LEFT);
+        delay(100);
+        Mouse.click(MOUSE_LEFT);
+        variavel4 = 0;
       }
     }
   } else {
     variavel4 = 1;
   }
-  //se o botao de clique longo ? apertado
+
+  // Controle de clique longo
   if (digitalRead(leftLong) == LOW) {
-    delay(DEBOUNCING);
-    if (digitalRead(leftLong) == LOW && variavel == 1) {
-      // muda o estado l?gico do bot?o
-      Mouse.press(MOUSE_LEFT);
-      variavel = !variavel;
-      variavel5 = !variavel5;
+    if (variavel == 1) {
+      delay(DEBOUNCING);
+      if (digitalRead(leftLong) == LOW) {
+        Mouse.press(MOUSE_LEFT);
+        variavel = 0;
+        variavel5 = 0;
+      }
     }
   } else {
-    variavel = 1;
-
-  }
-  if (variavel2 == 0 || variavel6 == 0) {
-    variavel5 = 1;
-  }
-  if (variavel2 == 1 && variavel5 == 1 && variavel6 == 1) {
-    Mouse.release(MOUSE_LEFT);
-  }
-
-  if ((digitalRead(velocidade) == LOW) && (espaco + 1000) < millis()) {
-    espaco = millis();
-    range = range + 2;
-    if (range > 8) {
-      range = 2;
+    if (variavel == 0) {
+      Mouse.release(MOUSE_LEFT);
+      variavel = 1;
+      variavel5 = 1;
     }
-    Serial.println(range);
   }
 
   delay(responseDelay);
